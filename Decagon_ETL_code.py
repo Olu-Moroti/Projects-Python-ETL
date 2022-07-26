61fe92e9faa52c3d741fe8fd62e4dc04160d68f6
Olumoroti Oshinowo <olumorotioshinowo@gmail.com>
2022-07-26 10:17:54 +0100

    first commit

---
+++ Decagon_ETL_code.py
@@ -0,0 +1,163 @@
+import requests
+import os
+from multiprocessing import Pool
+import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2
+
+# Get the data
+URLs = ['https://github.com/annexare/Countries/blob/master/data/continents.json',
+        'https://github.com/annexare/Countries/blob/master/data/countries.2to3.json',
+        'https://github.com/annexare/Countries/blob/master/data/countries.3to2.json',
+        'https://github.com/annexare/Countries/blob/master/data/countries.json',
+        'https://github.com/annexare/Countries/blob/master/data/languages.json']
+
+URLs = [i.replace('github.com', 'raw.githubusercontent.com').replace('/blob', '') for i in URLs]
+
+# Extract data
+for url in URLs:
+    filename = os.path.split(url)[1]
+    response = requests.get(url)
+    open(filename, 'wb').write(response.content)
+
+continents_df = pd.read_json('continents.json', orient='index')
+countries_2to3_df = pd.read_json('countries.2to3.json', orient='index')
+countries_3to2_df = pd.read_json('countries.3to2.json', orient='index')
+countries_df = pd.read_json('countries.json').transpose()
+languages_df = pd.read_json('languages.json').transpose()
+
+continents_df = continents_df.reset_index(0)
+countries_2to3_df = countries_2to3_df.reset_index(0)
+countries_3to2_df = countries_3to2_df.reset_index(0)
+countries_df = countries_df.reset_index(0)
+languages_df = languages_df.reset_index(0)
+
+# Login to database
+# print("Login to the localhost database.")
+dbname = 'postgres'
+username = 'postgres'
+password = 'admin'
+
+engine = create_engine(f'postgresql://{username}:{password}@localhost:5432/{dbname}')
+
+# Load data to database
+try:
+    continents_df.to_sql('CONTINENTS', con=engine)
+    countries_2to3_df.to_sql('COUNTRIES2TO3', con=engine)
+    countries_3to2_df.to_sql('COUNTRIES3TO2', con=engine)
+    countries_df.to_sql('COUNTRIES', con=engine)
+    languages_df.to_sql('LANGUAGES', con=engine)
+except ValueError: # TABLE NAME is already in the database
+    pass
+else:
+    pass
+
+query1 = """
+
+SELECT 
+    "CONTINENTS"."0" AS "Continent",
+    COUNT(*) AS "Country Count"
+FROM "COUNTRIES"
+LEFT JOIN "CONTINENTS" ON "CONTINENTS".index="COUNTRIES".continent 
+GROUP  BY 1
+
+"""
+
+query2 = """
+
+DROP TABLE IF EXISTS language_array;
+CREATE TABLE language_array AS (
+WITH delete_wrong_char AS (
+SELECT 
+    name,
+    rtrim(LTRIM(languages,'{'),'}') AS languages
+FROM "COUNTRIES"
+)
+SELECT 
+    name,
+    STRING_TO_ARRAY(TRIM(languages),',') AS languages
+FROM
+    delete_wrong_char
+);
+
+WITH single_language_cte AS (
+    SELECT 
+        name,
+        UNNEST(languages) AS each_language 
+    FROM language_array
+)
+SELECT 
+    "LANGUAGES".name AS "Language",
+    STRING_AGG(single_language_cte.name,', ') AS "Countries"
+FROM 
+    single_language_cte
+    LEFT JOIN "LANGUAGES" ON "LANGUAGES".index=single_language_cte.each_language
+GROUP BY 1;
+
+
+"""
+
+query3 = """
+
+DROP TABLE IF EXISTS language_array;
+CREATE TABLE language_array AS (
+WITH delete_wrong_char AS (
+SELECT 
+    name,
+    rtrim(LTRIM(languages,'{'),'}') AS languages
+FROM "COUNTRIES"
+)
+SELECT 
+    name,
+    STRING_TO_ARRAY(TRIM(languages),',') AS languages
+FROM
+    delete_wrong_char
+);
+
+SELECT 
+    name AS "Country",
+    COALESCE(ARRAY_LENGTH(languages,1),0) "Lng Count" 
+FROM language_array;
+
+"""
+
+queries = [query1, query2, query3]
+
+def  refresh_view(query):
+    conn = psycopg2.connect(database=dbname, user=username, password=password)
+    cur = conn.cursor()
+    view_name = query[10:15] # just random
+    cur.execute(query, (view_name,))
+    result = cur.fetchall()
+    conn.close()
+    return result
+
+if __name__ == '__main__':
+    p = Pool(processes=3)
+    view_names = ['view1','view2', 'view3']
+    result = p.map(refresh_view, queries)
+    result1 = result[0]
+    result2 = result[1]
+    result3 = result[2]
+
+    print("\n\nTask 1")
+    print("(Continent, Country Count)")
+    print("=============================================================")
+    for row in result1:
+        print(row)
+
+    print("\n\nTask 2")
+    print("(Language, Countries)")
+    print("=============================================================")
+    for row in result2:
+        print(row)
+
+    print("\n\nTask 3")
+    print("(Country, Lng Count)")
+    print("=============================================================")
+    for row in result3:
+        print(row)
+
+for file in os.listdir('.'):
+    if file.endswith('.json'):
+        os.remove(file)
---
+++ README.md
@@ -0,0 +1,35 @@
+# Task README
+
+## Summary
+The code in the file **code.py** gets a series of **json** files from a GitHub repository into a local PostgreSQL database, while also generating some reports.
+
+## Process
+The links of the json files were first gotten from GitHub directly, and then the links are copied into a list in the code.
+
+![Repository](img/Repository.jpg?raw=true "Title")
+
+The URLs strings were then reconstructed to reflect the "Raw" format of these json files in Github, so that they can be downloaded automatically with Python's `request` module.
+
+The files were then loaded to python with Pandas. A PostgreSQL server was created, and then the Pandas dataframe were converted to SQL and loaded into the PostgreSQL database.
+
+The downloaded json files are then deleted from the local computer.
+
+**Note**: You will have to edit the code to give it your custom **database name (dbname)**, **username** and **password** for your PostgreSQL **localhost** server.
+
+The remaining parts of the code is for some SQL report, as given in the tasks.
+
+### Task 1 Output
+![Task 1 Ouput](img/task1.jpeg?raw=true "Title")
+
+### Task 2 Output
+![Task 2 Ouput](img/task2.jpeg?raw=true "Title")
+
+### Task 3 Output
+![Task 3 Ouput](img/task3.jpg?raw=true "Title")
+
+## Libraries used
+
+- Pandas
+- NumPy
+- Sqlalchemy
+- Psycopg2
